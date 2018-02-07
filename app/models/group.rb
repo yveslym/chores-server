015417b6b179b360d8e4483eff7086748e988727
Sunny Ouyang <ShenOuyang.work@gmail.com>
2018-02-06 17:58:37 -0800

    added user tests

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
     has_many :chores
-    has_many :users
+    has_and_belongs_to_many :users
    # belongs_to :owner, class_name: :'User'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      user = User.new(
+        email: "Sunny@test.com",
+        password: "test123",
+        password_confirmation: "test123",
+      )
+      expect(user).to be_valid
+    end
+    it "is invalid without email" do
+      user = User.new(
+        email: nil,
+        password: "test123",
+        password_confirmation: "test123"
+      )
+      expect(user).to be_invalid
+    end
+    it "is invalid without password" do
+      user = User.new(
+        email: "Sunny@test.com",
+        password: nil,
+        password_confirmation: "test123"
+      )
+      expect(user).to be_invalid
+    end
+    it "is invalid without password_confirmation" do
+      user = User.new(
+        email: "Sunny@test.com",
+        password: "test123",
+        password_confirmation: nil
+      )
+      expect(user).to be_invalid
+    end
+  end
 end
