59c3caaf90fa5fc9ccbb38369762e3eb3ca203a0
yves songolo <yves.songolo@gmail.com>
2018-02-12 12:17:09 -0800

    add foreign key for group in chore model

--- db/migrate/20180207084351_add_chore_column_belongs_to_group.rb
+++ db/migrate/20180207084351_add_chore_column_belongs_to_group.rb
@@ -1,5 +1,5 @@
 class AddChoreColumnBelongsToGroup < ActiveRecord::Migration[5.1]
   def change
-      add_reference :chores, foreign_key: {to_table: :groups}
+      add_reference :chores, :group, index: :true
   end
 end
