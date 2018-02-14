1a0809b24eb48572b29885a0be53d7c785b5b015
yves songolo <yves.songolo@gmail.com>
2018-02-14 10:36:30 -0800

    some changes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ end
  post 'v1/new_account/' => 'v1/sessions#new_account'
 
  #chores routes
-  get 'v1/chores/:group_id/user/' => 'v1/chores#user_chores'
+  get 'v1/chores/user/' => 'v1/chores#user_chores'
 get 'v1/chores/:group_id/group/' => 'v1/chores#group_chores'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
