cdd8e193343fdfeac06e61c531224a9ce5a7836a
yves songolo <yves.songolo@gmail.com>
2018-03-15 13:35:23 -0700

    fix route

--- app/views/v1/requests/index.json.jbuilder
+++ app/views/v1/requests/index.json.jbuilder
@@ -5,7 +5,4 @@ json.array! @v1_request do |request|
 
 json.partial! 'v1/requests/request', request: request
 
-  # json.group do
-  # json.partial! 'v1/groups/group', group: group
-  #     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 #adds v1/ then the rest of the path
 namespace :v1, defaults: { format: :json }  do
-
-post 'new_account/' => 'sessions#new_account'
-
-#only create routes for things inside the array
 resources :requests
  # only: [:create, :index, :update]
 #if its resource, and not resources, there won't be id associated with the path
@@ -13,23 +9,25 @@ resource :sessions, only: [:create, :destroy,:show, :update]
 resources :groups do
     resources :chores
 end
+
+post 'new_account/' => 'sessions#new_account'
+#Route for sending out chore_completion requests for all users in the current group
+post 'chore_completion_request/' => 'requests#chore_completion_request'
+#Route for fetching all the group requests
+get 'fetch_group_requests/' => 'requests#fetch_group_requests'
+#Route for fetching all the chore completion requests
+get 'fetch_chore_completion_requests/' => 'requests#fetch_chore_completion_requests'
+
+#chores routes
+#For getting User in progress chores
+ get 'chores/user/' => 'chores#user_chores'
+ #Get User completed chores
+ get 'completed_chores/user/' => 'chores#completed_user_chores'
+ get 'chores/:group_id/group/' => 'chores#group_chores'
+ get 'groups/:group_id/completed_chores/' => 'chores#completed_group_chores'
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ #patch routes
+ patch 'groups/:id/user/' => 'groups#remove_member'
+#only create routes for things inside the array
 end
-
- #Route for sending out chore_completion requests for all users in the current group
- post 'v1/chore_completion_request/' => 'v1/requests#chore_completion_request'
- #Route for fetching all the group requests
- get 'v1/fetch_group_requests/' => 'v1/requests#fetch_group_requests'
- #Route for fetching all the chore completion requests
- get 'v1/fetch_chore_completion_requests/' => 'v1/requests#fetch_chore_completion_requests'
-
- #chores routes
- #For getting User in progress chores
-  get 'v1/chores/user/' => 'v1/chores#user_chores'
-  #Get User completed chores
-  get 'v1/completed_chores/user/' => 'v1/chores#completed_user_chores'
-  get 'v1/chores/:group_id/group/' => 'v1/chores#group_chores'
-  get 'v1/groups/:group_id/completed_chores/' => 'v1/chores#completed_group_chores'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #patch routes
-  patch 'v1/groups/:id/user/' => 'v1/groups#remove_member'
 end
